e026801ef2cd6f4c3ff848e0c2242ce7cdcf49ec
Pukar <68369744+Pukarojha@users.noreply.github.com>
2021-09-19 20:04:07 +0545

    a complete version

--- js/index.js
+++ js/index.js
@@ -1,16 +1,69 @@
 $(document).ready(function(){
     $('#menu').click(function(){
         $(this).toggleClass('fa-times');
-        $('.nav-bar').toggleClass('nav-toggle');
+        $('.navbar').toggleClass('nav-toggle'); 
     });
+
     $(window).on('scroll load', function(){
         $('#menu').removeClass('fa-times');
-        $('.nav-bar').removeClass('nav-toggle');
+        $('.navbar').removeClass('nav-toggle'); 
 
-        if($(window).scrollTop()>68){
+        if($(window).scrollTop() > 68){
             $('header .header-2').addClass('header-active');
         }else{
             $('header .header-2').removeClass('header-active');
         }
-    })
-})
+
+        $('section').each(function(){
+
+            let height= $(this).height();
+            let offset= $(this).offset().top -200;
+            let top= $(window).scrollTop();
+            let id= $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
+
+    });
+
+    // helix animation js starts Here
+    window.addEventListener("load", function () {
+        const Loader = document.querySelector(".Loader");
+        Loader.classList.add("hidden");  // Untag this when put in your website
+    });
+      
+    // helix animation js ends here
+
+    $('.home-slider').owlCarousel({
+        items:1,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout: 8000,
+        loop: true
+    });
+
+    $('.small-image img').click(function(){
+        $(this).addClass('image-active').siblings().removeClass('image-active');
+        let image= $(this).attr('src');
+
+        $('.big-image img').attr('src', image);
+    });
+
+    $('.gallery .btn').click(function(){
+
+        let filter= $(this).attr('data-filter');
+        if(filter=="all"){
+            $('.gallery .box').show(400);
+        }else{
+            $('.gallery .box').not('.' +filter).hide(200);
+            $('.gallery .box').filter('.' +filter).show(400);
+        }
+
+        $(this).addClass('button-active').sibblings().removeClass('button-active');
+        
+    });
+});
